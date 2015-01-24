95c1efe00e9178d70f8c5f27ec0acf0eeb5d810e
stbinan <stbinan@gmail.com>
2015-01-24 13:45:06 +0800

    destroy user

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   get 'static_pages/home'
 
   get 'static_pages/help'
-
-  resources :users
   root 'users#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
